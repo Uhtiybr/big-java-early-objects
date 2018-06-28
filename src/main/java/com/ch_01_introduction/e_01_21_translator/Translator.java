cca169c2b0cb064bf2e945ad74f98f7c68246089
Uhtiybr <uhtiybr90@gmail.com>
2018-06-28 16:24:36 +0300

    Add exercises

---
+++ src/main/java/com/ch_01_introduction/e_01_21_translator/Translator.java
@@ -0,0 +1,25 @@
+package main.java.com.ch_01_introduction.e_01_21_translator;
+
+/**
+ * Write a program
+ * that prints a two-column list with the greeting
+ * phrases shown in the table. In the first column,
+ * print the phrase in English, in the second col-
+ * umn, print the phrase in a language of your
+ * choice.
+ *
+ * @author Alexander Parinov
+ * @version 1.0
+ * @since 28.06.2018
+ */
+
+public class Translator {
+    public static void main(String[] args) {
+        System.out.println("Translator");
+        System.out.println("---------------------------------------------------------------------");
+        System.out.println("Good morning.                       Доброе утро.");
+        System.out.println("It is a pleasure to meet you.       Рад(а) знакомству.");
+        System.out.println("Please call me tomorrow.            Пожалуйста, позвоните мне завтра.");
+        System.out.println("Have a nice day!                    Приятного дня!");
+    }
+}
